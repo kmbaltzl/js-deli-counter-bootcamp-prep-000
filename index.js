32d83830ede00e5bab2f150e86a50fecd172edbf
Katie Baltzley <kmbaltzl@gmail.com>
2018-10-08 12:24:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeliLine = [];
 function takeANumber(line, name) {
+  
  var katzDeliLine = katzDeliLine.push(name)
  print ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.")
 }
