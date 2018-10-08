280914dfeee9fc6454f7bfec50648ff4f80687fe
Katie Baltzley <kmbaltzl@gmail.com>
2018-10-08 12:19:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,5 @@
+var katzDeliLine = [];
+function takeANumber(line, name) {
+ var katzDeliLine = katzDeliLine.push("Ada")
+ print ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.")
+}
